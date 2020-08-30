a4342a2d83051eae3dcd8d5d58680fa0bd3940de
Phil Gresham <philgresham@gmail.com>
2020-08-29 20:46:44 -0700

    Read instructions and push to Heroku the right way

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 const db = require('./config/keys').mongoURI;
@@ -11,6 +12,14 @@ require('./config/passport')(passport);
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (_req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 const keysProd = require('./keys_prod');
 const keysDev = require('./keys_dev');
+
 if (process.env.NODE_ENV === 'production') {
   module.exports = keysProd;
 } else {
