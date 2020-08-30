ba64c85397ee7d5d7f5db79c3c4bca39e5d22539
Phil Gresham <philgresham@gmail.com>
2020-08-29 20:42:03 -0700

    Try to fix Heroku deploy bug

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-/* eslint-disable global-require */
+const keysProd = require('./keys_prod');
+const keysDev = require('./keys_dev');
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
+  module.exports = keysProd;
 } else {
-  module.exports = require('./keys_dev');
+  module.exports = keysDev;
 }
